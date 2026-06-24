a43d493ad17d6cc7bf434b842dd92d9c058aff2d
Yahya Amin <yahyaamin1997@gmail.com>
2026-06-24 12:26:03 +0800

    added goto helper in BasePage and configured baseURL in config.ts

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -1,9 +1,17 @@
-import {Page} from '@playwright/test';
+import { Page } from '@playwright/test';
 
+// BasePage for other pages to extend and use the common functions
 export class BasePage {
     protected readonly page: Page;
 
     constructor(page: Page) {
         this.page = page;
     }
+
+    // Helper method to navigate to a specific path
+    async goto(path: string): Promise<void> {
+        await this.page.goto(path);
+    }
 }
+
+
